8679ab3204fbbadc274a4e78b5a8bef9b2fe4df8
Aqdas Malik <aqdasmalikcs@gmail.com>
2019-11-26 15:19:19 +0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mat-shift-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'MatShiftSelectionLibrary';
+}
